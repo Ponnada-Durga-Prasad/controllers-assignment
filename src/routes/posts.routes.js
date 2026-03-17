abb5d60c0387a76b66db99d391b804208eec0ea6
Durga Prasad Ponnada <durgaprasadponnada06@gmail.com>
2026-03-17 13:42:11 +0530

    Revert "refactor: move post logic to controller"
    This reverts commit 1b7c10bce063bfb6c8246d7c2aa0f33ccaaf91ac.

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,7 +1,3 @@
-const express = require("express");
-const postController = require("../controllers/posts.controller.js");
-const router = express.Router();
-
-router.get("/", (req, res) => postController.getAllPosts());
-
-module.exports = router;
+router.get("/", (req, res) => {
+  res.send("Fetching all blog posts...");
+});
